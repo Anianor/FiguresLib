215b0564f4f8af145ac13b9d7a2673d91d265e15
Anianor <91535133+Anianor@users.noreply.github.com>
2023-06-01 15:46:07 +0300

    Revert "Revert "Добавьте файлы проекта.""
    This reverts commit 9fca1a97cfb3af51d360f125367246389733f815.

---
+++ FigureLib2/Triangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiguresLib
+{
+    public interface  Triangle : IOperation
+    {
+        public bool IsRightTriangle();
+        public bool IsThisATriangle();
+
+      
+    }
+}
---
+++ FiguresLib/ITriangle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiguresLib
+{
+    public interface ITriangle :IOperation
+    {       
+        public bool IsRightTriangle();
+        public bool IsThisATriangle();
+    }
+}
